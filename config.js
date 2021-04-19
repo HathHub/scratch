ac4e097d363eb72458f2be1e1b5a77a1c00d213d
LautaroDeLaPuente <66932866+LautaroDeLaPuente@users.noreply.github.com>
2021-04-19 09:58:36 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-  token: "ODAzNTA5MDU1NzM2OTA1NzI4.YA-0Hw.zvfKxtGngUHPGOGSm8dad4eoFy8", // Reemplaze esto con el token de tu de bot.
-  prefix: "!", // El prefix de su bot para indentificar sus comandos.
-
-}
-
-// Aqui exportamos el objeto config:
-module.exports = config;
